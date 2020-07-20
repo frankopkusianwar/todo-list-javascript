2b914abe24c6f4a1ac213ce62c4ea2ca97722365
frankopkusianwar <okirorfrank3@gmail.com>
2020-07-20 12:58:20 +0300

    Add task class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import lists from './js/list'
+// import lists from './js/list'
 // import form from './js/list'
 
 
@@ -9,7 +9,25 @@ console.log('heeleo')
 
 // export { form }
 // export default lists()
+const task = (() => {
+  class Task {
+    constructor(title, description, date, notes,check) {
+      this.title = title
+      this.description = description
+      this.date = date
+      this.notes = notes
+      this.check = check
+    }
+  }
+  const form = document.querySelector('#task-form')
+  const addTask = (e) => {
+    e.preventDefault()
+    e.target.title.value
+  }
+  form.addEventListener('submit', addTask)
+  return form
+})()
 
 
-lists()
+// task()
 // form
