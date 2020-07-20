517649433a8cf302d0a67c5d72110a832c79726f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-20 18:20:07 +0530

    Add new tast functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const task = (() => {
   class Task {
-    constructor(title, description, date, notes,check) {
+    constructor(title, description, date, priority) {
       this.title = title
       this.description = description
       this.date = date
-      this.notes = notes
-      this.check = check
+      this.priority = priority
     }
   }
+
   const createButton = document.querySelector('.new-task-creator')
   const form = document.querySelector('#task-form')
   
@@ -16,25 +16,48 @@ const task = (() => {
     popUpForm.style.display = 'block'
   })
 
-
-
-// < div class="tasks" >
-//   <div class="task">
-//     <input type="checkbox" id="task-1" />
-//     <label for="task-1">
-//       <span class="custom-checkbox"></span>
-//       record todo list video
-//       </label>
-//     <p> soem sffssafdas</p>
-//     <p> 10-23-33</p>
-//     <p id='high'>High </p>
-//     <p><i class="fa fa-trash" aria-hidden="true"></i></p>
-//   </div>
-//   </div >
-
+  const addNewTask = (task) => {
+    const todoBody = document.querySelector('.todo-body')
+    const divTasks = document.createElement('div');
+    divTasks.setAttribute('class', 'tasks');
+    const divTask = document.createElement('div');
+    divTask.setAttribute('class', 'task');
+    const checkboxTask = document.createElement('input');
+    checkboxTask.setAttribute('type', 'checkbox');
+    checkboxTask.setAttribute('id', 'task-1');
+    const labelTask = document.createElement('label');
+    labelTask.setAttribute('for', 'task-1')
+    const spanTask = document.createElement('span');
+    spanTask.setAttribute('class', 'custom-checkbox')
+    labelTask.textContent = task.title
+    const descriptionTask = document.createElement('p');
+    descriptionTask.textContent = task.description
+    const dateTask = document.createElement('p');
+    dateTask.textContent = task.date
+    const priorityTask = document.createElement('p');
+    priorityTask.textContent = task.priority
+    const trashTask = document.createElement('p');
+  
+  
+    labelTask.appendChild(spanTask)
+    divTask.appendChild(checkboxTask)
+    divTask.appendChild(labelTask)
+    divTask.appendChild(descriptionTask)
+    divTask.appendChild(dateTask)
+    divTask.appendChild(priorityTask)
+    divTask.appendChild(trashTask)
+    todoBody.appendChild(divTask)
+  }
+  
 
   const addTask = (e) => {
     e.preventDefault()
+    const title =e.target.title.value;
+    const description =e.target.description.value;
+    const date =e.target.date.value;
+    const priority =e.target.priority.value;
+    const newTask = new Task(title, description, date, priority)
+    addNewTask(newTask)
   }
 
   form.addEventListener('submit', addTask)
