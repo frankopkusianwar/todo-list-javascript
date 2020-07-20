d7cc48d0637f6ee76c517fb8ed3fea4622860d3e
frankopkusianwar <okirorfrank3@gmail.com>
2020-07-20 11:26:08 +0300

    Refactor web-pack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,15 @@
-// Webpack uses this to work with directories
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
+
 module.exports = {
 
-  // Path to your entry point. From this file Webpack will begin his work
   entry: './src/index.js',
 
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -36,21 +31,15 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           {
-            // After all CSS loaders we use plugin to do his work.
-            // It gets all transformed CSS and extracts it into separate
-            // single bundled file
             loader: MiniCssExtractPlugin.loader
           },
           {
-            // This loader resolves url() and @imports inside CSS
             loader: "css-loader",
           },
           {
-            // Then we apply postCSS fixes like autoprefixer and minifying
             loader: "postcss-loader"
           },
           {
-            // First we transform SASS to standard CSS
             loader: "sass-loader",
             options: {
               implementation: require("sass")
@@ -77,10 +66,5 @@ module.exports = {
     compress: true,
     port: 8000
   },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript 
-  // minifying and other thing so let's set mode to development
   mode: 'development'
 };
